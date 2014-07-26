7b11c962d1c6e94c10d37c52cc0bd0c0eb32e54d
Tim Hobert <timhobert@yahoo.com>
2014-07-26 01:02:24 -0400

    HTML CSS and JS

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$.getJSON('data/questions.json', function(data) { 
+		var entry = data[Math.floor(Math.random()*data.length)];
+		// console.log(entry);
+
+		// output the result to html
+		var output = JSON.stringify(entry.Title);
+	    $('#question').html(output);
+	});
+
+	// big'ify
+	$("#question").fitText();
+
+});
